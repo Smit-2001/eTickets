7a3be0fddd4fade2db6747f99e1686db525ae6a2
Smit <smitpshah63@gmail.com>
2024-04-28 17:26:07 +0530

    Added EntityBaseRepository and ActorsService configuration with EntityBaseRepository

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,16 +1,12 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public interface IActorsService
-    {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);      
-        Task AddAsync(Actor actor);      
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);        
+    public interface IActorsService:IEntityBaseRepository<Actor>
+    {       
     }
 }
